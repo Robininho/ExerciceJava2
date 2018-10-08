ba4478bad3c1a8f8c7bce8ced0f162ec0ef00e78
Robininho <robin.halberda@orange.fr>
2018-10-08 16:14:01 +0200

    Uptdate

---
+++ js.js
@@ -0,0 +1,6 @@
+// Je créé 3 variables
+var firstName = "Robin";
+var lastName = "HALBERDA";
+var city = "Le Havre";
+// Affichage d'un message via Alert
+alert("Nom : " + firstName + "\nPrénom : " + lastName + "\nVille : " + city)
